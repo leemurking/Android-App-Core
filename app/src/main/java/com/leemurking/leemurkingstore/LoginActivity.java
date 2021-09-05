03412c8e9e0011ad84f3eaade031ee97d6be0fdd
leemurking <jamesgrant964@gmail.com>
2021-09-05 22:59:00 +0300

    Add the home Page of the Application

--- app/src/main/java/com/leemurking/leemurkingstore/LoginActivity.java
+++ app/src/main/java/com/leemurking/leemurkingstore/LoginActivity.java
@@ -2,13 +2,29 @@ package com.leemurking.leemurkingstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button loginButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton2 = (Button) findViewById(R.id.loginbutton2);
+        loginButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+    }
+    public void openLoginActivity() {
+        Intent intent = new Intent(this, HomePageActivity.class);
+        startActivity(intent);
     }
 }
