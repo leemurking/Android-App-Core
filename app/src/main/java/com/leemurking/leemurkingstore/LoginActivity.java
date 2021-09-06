bd4183f8b609095201fa36b3fcaa8ab27ca7982b
leemurking <jamesgrant964@gmail.com>
2021-09-06 14:01:15 +0300

    Add validations to the login and sign up forms

--- app/src/main/java/com/leemurking/leemurkingstore/LoginActivity.java
+++ app/src/main/java/com/leemurking/leemurkingstore/LoginActivity.java
@@ -6,22 +6,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 
 public class LoginActivity extends AppCompatActivity {
     Button newloginbutton;
     Button signupbutton;
+
+    EditText editusername,editpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        editusername = findViewById(R.id.editusername);
+        editpassword = findViewById(R.id.editpassword);
 
         newloginbutton = (Button) findViewById(R.id.newloginbutton);
+
+        AwesomeValidation awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        awesomeValidation.addValidation(this,R.id.editusername, RegexTemplate.NOT_EMPTY,R.string.invalid_name);
+
+        awesomeValidation.addValidation(this, R.id.editpassword,
+                ".{6}", R.string.invalid_password);
+
+
+
+
         newloginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(awesomeValidation.validate()) {
+                    Toast.makeText(getApplicationContext() ,
+                            "Form Valodate Successfully...",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext()
+                    ,"Validation Failed,",Toast.LENGTH_SHORT).show();
+                }
+
+
                 Intent intent = new Intent(LoginActivity.this, HomePageActivity.class);
                 startActivity(intent);
             }
--- app/src/main/java/com/leemurking/leemurkingstore/SignUpActivity.java
+++ app/src/main/java/com/leemurking/leemurkingstore/SignUpActivity.java
@@ -6,19 +6,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 
 public class SignUpActivity extends AppCompatActivity {
     Button signupbutton;
+
+    EditText signupeditusername,signupeditpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        signupeditusername = findViewById(R.id.signupeditusername);
+        signupeditpassword = findViewById(R.id.signupeditpassword);
+
         signupbutton = findViewById(R.id.signupbutton);
+
+        AwesomeValidation awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        awesomeValidation.addValidation(this,R.id.signupeditusername, RegexTemplate.NOT_EMPTY,R.string.invalid_name);
+
+        awesomeValidation.addValidation(this, R.id.signupeditpassword,
+                ".{6}", R.string.invalid_password);
+
         signupbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(awesomeValidation.validate()) {
+                    Toast.makeText(getApplicationContext() ,
+                            "Form Valodate Successfully...",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext()
+                            ,"Validation Failed,",Toast.LENGTH_SHORT).show();
+                }
+
                 Intent intent = new Intent(SignUpActivity.this, HomePageActivity.class);
                 startActivity(intent);
             }
