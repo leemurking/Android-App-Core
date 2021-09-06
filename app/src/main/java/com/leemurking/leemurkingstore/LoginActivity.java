3bb150223b65913bb7ae8b421a8e9b3fd3a454ae
leemurking <jamesgrant964@gmail.com>
2021-09-06 12:37:26 +0300

    Add the signup Activity

--- app/src/main/java/com/leemurking/leemurkingstore/LoginActivity.java
+++ app/src/main/java/com/leemurking/leemurkingstore/LoginActivity.java
@@ -8,12 +8,34 @@ import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button loginButton2;
+    Button newloginbutton;
+    Button signupbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+        newloginbutton = (Button) findViewById(R.id.newloginbutton);
+        newloginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HomePageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signupbutton = (Button) findViewById(R.id.signupbutton);
+        signupbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
